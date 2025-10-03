50d1d3311d0559f85d27fcb0cdcd9a978353b8b2
Josias <josias.sd9@gmail.com>
2025-10-03 17:46:41 +0000

    está tudo perfeito. Agora gostaria que você me indicasse, a partir de to

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <meta name="theme-color" content="#3a3a3a" />
       </head>
       <body className="font-body antialiased">
         {children}

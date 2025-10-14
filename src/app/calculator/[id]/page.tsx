d2f674a5e76ef341255df068f36e48b1153d666d
Josias <josias.sd9@gmail.com>
2025-10-14 01:22:26 +0000

    Try fixing this error: `Console Error: A param property was accessed dir

--- src/app/calculator/[id]/page.tsx
+++ src/app/calculator/[id]/page.tsx
@@ -4,11 +4,11 @@
 import { Dashboard } from "@/components/dashboard";
 import * as React from "react";
 
-export default function CalculatorPage({ params: { id } }: { params: { id: string } }) {
+export default function CalculatorPage({ params }: { params: { id: string } }) {
   return (
     <>
       <main className="min-h-screen bg-background">
-          <Dashboard initialCategoryId={id} />
+          <Dashboard initialCategoryId={params.id} />
       </main>
     </>
   );

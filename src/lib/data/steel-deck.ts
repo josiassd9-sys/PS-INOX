777ec08f53bfafd38699e25166845a905f38375d
Josias <josias.sd9@gmail.com>
2025-10-17 19:45:07 +0000

    na tabela de Steel Deck, poderia completar com espessuras disponíveis no

--- src/lib/data/steel-deck.ts
+++ src/lib/data/steel-deck.ts
@@ -23,6 +23,16 @@ export const steelDeckData: SteelDeck[] = [
         }
     },
     { 
+        nome: "MD57 - Chapa 1.25mm", 
+        tipo: 'MD57',
+        espessuraChapa: 1.25, 
+        pesoProprio: 15.54, 
+        vaosMaximos: {
+            simples: { '150kgf': 3.20, '250kgf': 2.90, '400kgf': 2.55 },
+            duplo: { '150kgf': 4.15, '250kgf': 3.75, '400kgf': 3.30 }
+        }
+    },
+    { 
         nome: "MD75 - Chapa 0.80mm", 
         tipo: 'MD75',
         espessuraChapa: 0.80, 
@@ -40,6 +50,16 @@ export const steelDeckData: SteelDeck[] = [
         vaosMaximos: {
             simples: { '150kgf': 3.45, '250kgf': 3.15, '400kgf': 2.75 },
             duplo: { '150kgf': 4.50, '250kgf': 4.05, '400kgf': 3.55 }
+        }
+    },
+    { 
+        nome: "MD75 - Chapa 1.25mm", 
+        tipo: 'MD75',
+        espessuraChapa: 1.25, 
+        pesoProprio: 16.63, 
+        vaosMaximos: {
+            simples: { '150kgf': 3.80, '250kgf': 3.45, '400kgf': 3.00 },
+            duplo: { '150kgf': 4.95, '250kgf': 4.45, '400kgf': 3.90 }
         }
     },
 ];
